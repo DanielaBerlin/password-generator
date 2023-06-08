79a07dac06a10bd5ba9b9b40d9c4ea1828d04798
Daniela <danielacardona2203@gmail.com>
2023-06-08 16:37:29 +0200

    Add variables to store user input for uppercase, lowercase, symbol, and number preferences

---
+++ script.js
@@ -0,0 +1,13 @@
+const empty = "",
+uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+lCase = "abcdefghijklmnopqrstuvwxyz",
+number = "0123456789",
+symbol = "!@#$%^&*=-_";
+
+const pLength = dcoument.getElementById("p-length"),
+upperCase = dcoument.getElementById("p-uppercase"),
+lowerCase = dcoument.getElementById("p-lowercase"),
+pNumber = dcoument.getElementById("p-number"),
+pSymbol = dcoument.getElementById("p-symbol"),
+submit = dcoument.getElementById("submit"),
+password = dcoument.getElementById("password");
